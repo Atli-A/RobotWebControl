21e636c5ee24534eb04bf3a55550a9cfc52303e6
Atli Arnarsson <atli.nikulas@gmail.com>
2021-01-09 16:49:44 -0800

    minor bug fixes

--- sendToArduino.py
+++ sendToArduino.py
@@ -3,23 +3,24 @@ import rospy
 from std_msgs.msg import String
 
 
+#tests
+# testjson = [{"positions": [90, 90, 90, 90, 151, 134]}]
 
-testjson = [{"positions": [90, 90, 90, 90, 151, 134]}]
 
-
-for i in testjson:
-    print('\n')
-    print(i['positions'])
+# for i in testjson:
+#     print('\n')
+#     print(i['positions'])
 
 rospy.init_node("customPub")
 
 firstCmdPart = '{"command":"direct","v1":'
+publisher = rospy.Publisher('/evocar/pub', String, queue_size=5)
 
-publisher = rospy.Publisher('/evocar/status', String, queue_size=5)
 
 
 
 def publish(jsonInput):
+    global publisher
     publisher.publish( String('{"command":"direct","v1":"L:0,R:0,f:0.4"}'))
 
 #data: "{\"command\":\"direct\",\"v1\":\"L:0,R:0,x:-1.47\"}"

56332317e20c37bd7d8b10c80a2a6056d56f80a6
Atli-A <atli.nikulas@gmail.com>
2021-01-09 23:19:49 -0800

    changes that didn't work

--- sendToArduino.py
+++ sendToArduino.py
@@ -24,11 +24,21 @@ publisher = rospy.Publisher('/evocar/pub', String, queue_size=5)
 
 
 def decodeJson(jsonInput):
-    arr = json.loads(jsonInput)
-    for i in jsonInput:
-        arr = i['positions']
+#    arr = json.dumps(jsonInput)
+    arr = jsonInput 
+    print(type(arr))
+    print(arr)
+    print(arr)
+
+    print(arr)
+    print(arr)
+    print(arr)
+    res = 0
+    for i in arr:
+        res= arr['positions']
     
-    return arr
+    print(res)
+    return res
 
 
 def publish(jsonInput):
@@ -36,19 +46,19 @@ def publish(jsonInput):
     global publisher
     arr = decodeJson(jsonInput)
 
-    for i in range(arr.len):
+    for i in range(len(arr)):
         arr[i] = int(arr[i])
     
     
     mainstring = firstCmdPart
 
-    for i in len(jsonInput):
-        mainstring += chr(i + 97) + ":" + arr[i] + ","
+    for i in range(len(jsonInput)):
+        mainstring += chr(i + 97) + ":" + str(arr[i]) + ","
 
     mainstring += lastCmdPart
-    # publisher.publish( String('{"command":"direct","v1":"L:0,R:0,f:+0.8"}'))
+    #publisher.publish( String('{"command":"direct","v1":"L:0,R:0,f:+0.8"}'))
     publisher.publish(String(mainstring))
-
+    print("published")
     #example publish
-    # publisher.publish(String(
-    #     '{"h": 147.43, "r": 233.14, "pwr": true, "pos": "a:90,b:88.19,c:108.62,d:90,e:90.87,f:94.80,pwr:1", "v": 70.44}'))
+    #publisher.publish(String(
+    #    '{"h": 147.43, "r": 233.14, "pwr": true, "pos": "a:50,b:88.19,c:108.62,d:90,e:90.87,f:94.80,pwr:1", "v": 70.44}'))

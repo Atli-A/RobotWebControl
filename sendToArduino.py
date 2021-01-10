13656db7bc62705cf10bafd040e0612981800000
Atli-A <atli.nikulas@gmail.com>
2021-01-09 16:50:10 -0800

    minor changes

--- sendToArduino.py
+++ sendToArduino.py
@@ -3,24 +3,25 @@ import rospy
 from std_msgs.msg import String
 
 
+#
+#testjson = [{"positions": [90, 90, 90, 90, 151, 134]}]
 
-testjson = [{"positions": [90, 90, 90, 90, 151, 134]}]
 
-
-for i in testjson:
-    print('\n')
-    print(i['positions'])
+#for i in testjson:
+#    print('\n')
+#    print(i['positions'])
 
 rospy.init_node("customPub")
 
-firstCmdPart = '{"command":"direct","v1":'
+#firstCmdPart = '{"command":"direct","v1":'
 
-publisher = rospy.Publisher('/evocar/status', String, queue_size=5)
+publisher = rospy.Publisher('/evocar/pub', String, queue_size=5)
 
 
 
 def publish(jsonInput):
-    publisher.publish( String('{"command":"direct","v1":"L:0,R:0,f:0.4"}'))
+    print("recieved")
+    publisher.publish( String('{"command":"direct","v1":"L:0,R:0,x:+0.8"}'))
 
 #data: "{\"command\":\"direct\",\"v1\":\"L:0,R:0,x:-1.47\"}"
 

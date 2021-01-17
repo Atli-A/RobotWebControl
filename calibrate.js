bf3b98d3e9856adfab1dcf2901d56151ce0fc66a
Atli Arnarsson <atli.nikulas@gmail.com>
2021-01-17 14:23:20 -0800

    started calibration page

---
+++ calibrate.js
@@ -0,0 +1,6 @@
+let instructions = document.getElementById("instructions");
+
+
+let motorList = ["a", "b", "c", "d", "e", "f"]
+
+instructions.innerHTML = "Hold space until motor " + motorList + " stops"

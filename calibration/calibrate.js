c2cba325c1677fc612eb0a52b9f1aeefc36cdb12
Atli Arnarsson <atli.nikulas@gmail.com>
2021-01-20 20:26:28 -0800

    worked on calibration + css

--- calibration/calibrate.js
+++ calibration/calibrate.js
@@ -1,6 +1,53 @@
 let instructions = document.getElementById("instructions");
 
 
-let motorList = ["a", "b", "c", "d", "e", "f"]
+let motorList = ["A", "B", "C", "D", "E", "F"]
 
-instructions.innerHTML = "Hold space until motor " + motorList + " stops"
+let currentMotor = 0
+
+instructions.innerHTML = "Hold space until motor " + motorList[currentMotor] + " stops"
+
+
+var r = document.querySelector(':root');
+
+let spaceDown = false;
+
+
+
+/* functions */
+function setBGto(nameOfBG) {
+
+    if (nameOfBG === "dark") {
+        r.style.setProperty('--backColor', '#888');
+
+    }
+    else if (nameOfBG === "light") {
+        r.style.setProperty('--backColor', '#ccc');
+
+    }
+    else {
+        console.error("Warning: color " + nameOfBG + " is not recognized")
+    }
+}
+
+/* check if space key is down */
+document.body.onkeyup = function (e) {
+    if (e.keyCode == 32) {
+        spaceDown = false
+        setBGto("light")
+    }
+}
+
+
+document.body.onkeydown = function (e) {
+    if (e.keyCode == 32) {
+        spaceDown = true
+        setBGto("dark")
+    }
+}
+
+
+
+function calibrationSequence() {
+    
+}

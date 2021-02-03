1fa2f3beaef0dece04b3f840a123ebf91abcd5bf
Atli-A <atli.nikulas@gmail.com>
2021-02-02 23:34:04 -0800

    fixed input bug

--- server.py
+++ server.py
@@ -55,7 +55,7 @@ def status_read(ros_data):
     #print(temp_arr)
     for i in range(6):
         #print(temp_arr[i])
-        current_pos.append(temp_arr[chr(i+97)[i]])
+        current_pos.append(temp_arr[chr(i+97)])
 
         pass
 

2931e5a30c4d2f1e5523f3bc47c4ae84d2ee74d2
Atli-A <atli.nikulas@gmail.com>
2021-02-02 23:06:02 -0800

    cleaned up sta

--- send_to_arduino.py
+++ send_to_arduino.py
@@ -3,16 +3,6 @@ import math
 import threading
 import rospy
 from std_msgs.msg import String
-# tests
-# testjson = [{"positions": [90, 90, 90, 90, 151, 134]}]
-
-
-# for i in testjson:
-#     print('\n')
-#     i['positions'])
-
-
-#rospy.init_node("customPub")
 
 
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f']
@@ -20,17 +10,12 @@ zeroes = [90, 90, 90, 90, 90, 90]
 
 
 
-# firstCmdPart = '{"h": 147.43, "r": 233.14, "pwr": true, "pos": "'
-# lastCmdPart = 'pwr:1", "v": 70.44}'
 publisher = rospy.Publisher('/evocar/pub', String, queue_size=5)
 
 firstCmdPart = '{"command":"direct","v1":"L:0,R:0,'
 lastCmdPart = '"}'
 
 
-
-# Example string:
-#       '{"h": 147.43, "r": 233.14, "pwr": true, "pos": "a:90,b:88.19,c:108.62,d:90,e:90.87,f:94.80,pwr:1", "v": 70.44}'
 def printDict(dictInput):
     result = ""
     for i in dictInput:
@@ -46,18 +31,12 @@ def getSign(numInput):
 
 def decodeJson(jsonInput):
     print(type(jsonInput))
-    #arr = json.loads(jsonInput)
     arr = jsonInput
     print("type of arr is " + str(type(arr)))
     arr = arr["positions"]
-   #print("arr = " + str(arr))
-
-   #print("arr is a " + str(type(arr)))
-   #res = 0
     for i in range(len(arr)):
        print(i)
        arr[i] = float(arr[i])
-    #print(res)
     return arr
 
 
@@ -85,13 +64,6 @@ def publish(jsonInput, current_pos):
     print("listDelta = " + str(listDelta))
 
     zeroes = list
-    #old code form before delta was being calculated
-   # print("jsonInput = " + printDict(list))
-    #for i in range(len(list)):
-    #    tmp = chr(i + 97) + ":" + getSign(int(list[i])-90) + str((list[i]-90 )/10) + ","
-    #    mainstring += tmp
-    #    print(tmp)
-    #    print("letter: " + chr(i + 97))
    
     for i in range(len(listDelta)):
         if (round(listDelta[i]) != 0):
@@ -109,26 +81,5 @@ def publish(jsonInput, current_pos):
    #this doesnt work i think
     if (String(mainstring) != firstCmdPart + lastCmdPart):
         publisher.publish(String(mainstring))
-    # publisher.publish('data: "{\"command\":\"action\",\"v1\":\"k\"}"')
-    # print("published")
-    #example publish
-   # publisher.publish(String(
-   #     '{"h": 147.43, "r": 233.14, "pwr": true, "pos": "a:50,b:88.19,c:108.62,d:90,e:90.87,f:94.80,pwr:1", "v": 70.44}'))
-
-#def listenthread():
-    #while (True):
-       # print("test")
-    #    pass
-
-
-#def rosthread():
-  #  rospy.spin()
-
-#ros_thread = threading.Thread(target=rosthread, name="ros")
-
-#listent = threading.Thread(target=listenthread, name="listen")
-#listent.daemon = True
-#listent.start()
-#rospy.spin()
-
+    
 print("made to end")
